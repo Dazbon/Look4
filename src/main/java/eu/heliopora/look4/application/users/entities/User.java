f4636b335fb0915ce5a37313f28ab3abe5b664aa
suplik10 <ssuplik@seznam.cz>
2014-03-14 14:48:52 +0100

    Entites
    user jsem jsen zformátoval,když už jsem v tom byl, aby to bylo jednotný
    :)

--- src/main/java/eu/heliopora/look4/application/users/entities/User.java
+++ src/main/java/eu/heliopora/look4/application/users/entities/User.java
@@ -11,33 +11,35 @@ import eu.heliopora.look4.commons.interfaces.IBuilder;
 public class User extends BasePersistentObjectImpl implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	// If you modify these fields in any way, you have to modify Builder's fields and its build() method as well
+
+	// If you modify these fields in any way, you have to modify Builder's
+	// fields and its build() method as well
 
 	UserAuthenticationDetail userAuthenticationDetail;
 
 	UserBusinessDetail userBusinessDetail;
-	
+
 	// No-arguments constructor required by JPA
-	
+
 	protected User() {
 
 	}
-	
+
 	// Instances can only be created via Builder
-	
+
 	private User(Builder builder) {
 		this.userAuthenticationDetail = builder.userAuthenticationDetail;
 		this.userBusinessDetail = builder.userBusinessDetail;
 	}
-	
+
 	// Builder
-	
+
 	public static class Builder implements IBuilder<User> {
 		private UserAuthenticationDetail userAuthenticationDetail;
 		private UserBusinessDetail userBusinessDetail;
 
-		public Builder userAuthenticationDetails(UserAuthenticationDetail userAuthenticationDetail) {
+		public Builder userAuthenticationDetails(
+				UserAuthenticationDetail userAuthenticationDetail) {
 			this.userAuthenticationDetail = userAuthenticationDetail;
 			return this;
 		}
@@ -47,8 +49,9 @@ public class User extends BasePersistentObjectImpl implements Serializable {
 			return this;
 		}
 
-		// Validation has to be performed on a newly created instance, not on Builder's fields (Builder is not thread-safe).
-		
+		// Validation has to be performed on a newly created instance, not on
+		// Builder's fields (Builder is not thread-safe).
+
 		public User build() {
 			User user = new User(this);
 			Validate.notNull(user.getUserAuthenticationDetail());
@@ -56,14 +59,15 @@ public class User extends BasePersistentObjectImpl implements Serializable {
 			return user;
 		}
 	}
-	
+
 	// Getters and Setters
-	
+
 	public UserAuthenticationDetail getUserAuthenticationDetail() {
 		return userAuthenticationDetail;
 	}
 
-	public void setUserAuthenticationDetail(UserAuthenticationDetail userAuthenticationDetail) {
+	public void setUserAuthenticationDetail(
+			UserAuthenticationDetail userAuthenticationDetail) {
 		this.userAuthenticationDetail = userAuthenticationDetail;
 	}
 
@@ -74,9 +78,10 @@ public class User extends BasePersistentObjectImpl implements Serializable {
 	public void setUserBusinessDetail(UserBusinessDetail userBusinessDetail) {
 		this.userBusinessDetail = userBusinessDetail;
 	}
-	
-	// For code brevity, toString() method is created via reflection. You can provide custom implementation, if needed.
-	
+
+	// For code brevity, toString() method is created via reflection. You can
+	// provide custom implementation, if needed.
+
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
--- src/main/java/eu/heliopora/look4/tracking/entities/TrackingDetail.java
+++ src/main/java/eu/heliopora/look4/tracking/entities/TrackingDetail.java
@@ -1,9 +1,73 @@
 package eu.heliopora.look4.tracking.entities;
 
-public class TrackingDetail {
-	
-	private TrackingDevice trackingDevice;
-	
-	// Maybe other fields in future
+import java.io.Serializable;
 
+import javax.sound.midi.Track;
+
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import eu.heliopora.look4.commons.entities.base.BasePersistentObjectImpl;
+import eu.heliopora.look4.commons.interfaces.IBuilder;
+import eu.heliopora.look4.tracking.entities.Resource.Builder;
+
+public class TrackingDetail extends BasePersistentObjectImpl implements
+		Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// If you modify these fields in any way, you have to modify Builder's
+	// fields and its build() method as well
+
+	private TrackingDevice trackingDevice;
+
+	// No-arguments constructor required by JPA
+
+	public TrackingDetail() {
+
+	}
+
+	// Instances can only be created via Builder
+
+	private TrackingDetail(Builder builder) {
+		this.trackingDevice = builder.trackingDevice;
+	}
+
+	// Builder
+
+	public static class Builder implements IBuilder<TrackingDetail> {
+		private TrackingDevice trackingDevice;
+
+		public Builder trackingDevice(TrackingDevice trackingDevice) {
+			this.trackingDevice = trackingDevice;
+			return this;
+		}
+
+		public TrackingDetail build() {
+			TrackingDetail trackingDetail = new TrackingDetail(this);
+			Validate.notNull(trackingDetail.getTrackingDevice());
+			return trackingDetail;
+		}
+	}
+
+	// Validation has to be performed on a newly created instance, not on
+	// Builder's fields (Builder is not thread-safe).
+
+	// Getters and Setters
+
+	public TrackingDevice getTrackingDevice() {
+		return trackingDevice;
+	}
+
+	public void setTrackingDevice(TrackingDevice trackingDevice) {
+		this.trackingDevice = trackingDevice;
+	}
+
+	// For code brevity, toString() method is created via reflection. You can
+	// provide custom implementation, if needed.
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 }
