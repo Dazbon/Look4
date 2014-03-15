5b1b09f0b08442fcc92e838a913965e9c6c6e120
Dazbon <lukas@Lukass-MacBook-Pro.local>
2014-03-15 23:44:26 +0100

    Revert "Piatok, sobota"
    This reverts commit 226c67ba8b81413d939ad882640f20d616891815.

--- src/main/java/eu/heliopora/look4/business/objects/entities/Vehicle.java
+++
@@ -1,7 +0,0 @@
-package eu.heliopora.look4.business.objects.entities;
-
-public class Vehicle {
-	
-	// Empty for now, created just for illustration
-
-}
--- src/main/java/eu/heliopora/look4/commons/utils/ApplicationProperties.java
+++
@@ -1,7 +0,0 @@
-package eu.heliopora.look4.commons.utils;
-
-public class ApplicationProperties {
-	
-	private static final String APPLICATION_PROPERTIES = "application.properties";
-
-}
